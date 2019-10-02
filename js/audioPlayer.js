4eac9bd534c32eaa469a266d07c627ef5d76da28
shalabhnair <41040282+shalabhnair@users.noreply.github.com>
2019-10-02 15:13:15 +0530

    Add files via upload

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -20,47 +20,48 @@ jQuery(function ($) {
         // initialize playlist and controls
         var index = 0,
             playing = false,
+            mediaPath = '',
             extension = '',
             tracks = [{
                 "track": 1,
                 "name": "Enundodee",
                 "duration": "4:00",
-                "file": "http://malayalammp3.a2z3gp.com/down.php?file=load/Malayalam%20mp3/Malayalam%202013%20MP3%20Songs/Celluloid%20-%20%20(2013)/Enundodi.mp3"
+                "file": "https://malayalammp3.a2z3gp.com/down.php?file=load/Malayalam%20mp3/Malayalam%202013%20MP3%20Songs/Celluloid%20-%20%20(2013)/Enundodi"
             }, {
                 "track": 2,
                 "name": "O Ponthoovalaayi",
                 "duration": "8:30",
-                "file": "https://www.dropbox.com/s/d05gwfam4hqsu8u/Ee%20adutha%20kaalathu-O%20Ponthoovalai%28Sithara%29.mp3"
-            }, {
+                "file": "audio/Ee%20adutha%20kaalathu-O%20Ponthoovalai%28Sithara%29"
+            },  {
                 "track": 3,
                 "name": "Kangal Neeye",
                 "duration": "5:01",
-                "file": "https://www.dropbox.com/s/li6is3ntit7fp3g/Muppoluthum%20un%20karpanaigal-Kangal%20Neeyae%28Sithara%29.mp3"
+                "file": "audio/Muppoluthum%20un%20karpanaigal-Kangal%20Neeyae%28Sithara%29"
             }, {
                 "track": 4,
                 "name": "Azhake Azhake Minal",
                 "duration": "8:31",
-                "file": "https://www.dropbox.com/s/mawtx6hbiqu0byy/Azhake%20Azhake.mp3"
+                "file": "audio/Azhake%20Azhake"
             }, {
                 "track": 5,
                 "name": "Panchara Chiri Kondu",
                 "duration": "5:05",
-                "file": "https://www.dropbox.com/s/2p1gtuczgb29l3u/Marykkundoru%20kujaadu-Pancharachiri%20Kondu%28Sithara%2C%20Franco%29.mp3"
+                "file": "audio/Marykkundoru%20kujaadu-Pancharachiri%20Kondu%28Sithara%2C%20Franco%29"
             }, {
                 "track": 6,
                 "name": "Hoyyuthuthe",
                 "duration": "2:48",
-                "file": "https://www.dropbox.com/s/8swvvj8ky40sai7/Aidhu%20ondla%20aidu-Hoyyuthuthe%20malai%28Sithara%29.mp3"
+                "file": "audio/Aidhu%20ondla%20aidu-Hoyyuthuthe%20malai%28Sithara%29"
             }, {
                 "track": 7,
                 "name": "Hath Le Le",
                 "duration": "5:44",
-                "file": "https://www.dropbox.com/s/li6q0uuikiyv2op/Mayamohini-Haath%20lele%28Sithara%29.mp3"
+                "file": "audio/Mayamohini-Haath%20lele%28Sithara%29"
             }, {
                 "track": 8,
                 "name": "Aathadi",
                 "duration": "5:26",
-                "file": "https://www.dropbox.com/s/wvg2r02vmqsrc4t/Krishnaveni%20Panjalai%20-%20Aathaadi%28Sithara%2CHarish%20raghavendra%29.mp3"
+                "file": "audio/Krishnaveni%20Panjalai%20-%20Aathaadi%28Sithara%2CHarish%20raghavendra%29"
             }],
             buildPlaylist = $.each(tracks, function(key, value) {
                 var trackNumber = value.track,
@@ -135,7 +136,7 @@ jQuery(function ($) {
                 $('#plList li:eq(' + id + ')').addClass('plSel');
                 npTitle.text(tracks[id].name);
                 index = id;
-                audio.src = tracks[id].file;
+                audio.src = mediaPath + tracks[id].file + extension;
                 updateDownload(id, audio.src);
             },
             updateDownload = function (id, source) {
@@ -147,7 +148,7 @@ jQuery(function ($) {
                 loadTrack(id);
                 audio.play();
             };
-        //extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
         loadTrack(index);
     } else {
         // no audio support
